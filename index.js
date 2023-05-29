9141ae481042bc26a68ee8089556d09fab22a1fb
soumya <119067178+soumyasourav139@users.noreply.github.com>
2023-05-29 23:29:38 +0530

    done

---
+++ index.js
@@ -0,0 +1,52 @@
+const dropdownEl = document.getElementById("colors");
+const getBtn = document.getElementById("get-btn");
+let colorsArray = [];
+window.addEventListener("DOMContentLoaded", () => {
+  renderColors();
+});
+getBtn.addEventListener("click", renderColors);
+
+function renderColors() {
+  let selectedColor = document.getElementById("color-picker").value;
+  selectedColor = selectedColor
+    .split("")
+    .filter((char) => {
+      return char != "#";
+    })
+    .join("");
+  let scheme = dropdownEl.value;
+
+  console.log(selectedColor);
+  let url = `https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${scheme}&count=5`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      let schemeColors = data.colors;
+      let colorsArray = schemeColors.map((color) => {
+        return color.hex.value;
+      });
+      let sectionEl = document.getElementById("section");
+      let colorsHtml = "";
+      for (hex of colorsArray) {
+        colorsHtml += `
+                <div class="rectangle" id = '${hex}'>
+                    <div class="color" style = 'background: ${hex}'></div>
+                    <div class="color-hex">${hex}</div>
+                </div>`;
+      }
+      sectionEl.innerHTML = colorsHtml;
+
+      let array = document.getElementsByClassName("rectangle");
+      for (let i = 0; i < 5; i++) {
+            array[i].addEventListener("click", () => {
+            navigator.clipboard.writeText(array[i].id);
+            let popup = document.getElementById("popup");
+            popup.style.display = "block";
+            setTimeout(() => {
+            popup.style.display = "none";
+          }, 1000);
+        });
+      }
+    });
+}
